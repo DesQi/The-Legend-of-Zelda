6868c6d4a09e5293ae72e22dcbe07343f8277ba7
qidesmond <qidesmond@gmail.com>
2016-09-16 11:36:28 -0400

    ruby test

---
+++ Assets/player_control.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_control : MonoBehaviour {
+
+	public float walking_volocity = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal_input =  Input.GetAxis ("Horizontal");
+		float vertical_input =  Input.GetAxis ("Vertical");
+
+		if (horizontal_input != 0.0f)
+			vertical_input = 0.0f;
+
+		GetComponent<Rigidbody> ().velocity = new Vector3 (horizontal_input, vertical_input, 0) * walking_volocity;
+	}
+
+	void OnTriggerEnter(Collider coll) {
+		print (coll.gameObject.name);
+	}
+}
