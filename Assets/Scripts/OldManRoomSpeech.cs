fa9cc70fa9d89d7fee4a2b96fc5618abf6eb1027
snowicecat <snowicecat@126.com>
2016-09-20 22:06:59 -0400

    Old Man Speech Complete

---
+++ Assets/Scripts/OldManRoomSpeech.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OldManRoomSpeech : MonoBehaviour {
+
+	public static OldManRoomSpeech instance;
+	public bool speechTrigger = false;
+	public string[] textArr;
+	public int idxOfText = 0;
+	public Vector3 OldManRoomCameraPos = new Vector3(7.51f, 39.41f, -10.0f);
+	public int counter = 8;
+	// Use this for initialization
+	void Start () {
+		if (instance != null)
+			Debug.LogError ("Multiple speech instance.");
+		instance = this;
+		print (textArr.Length);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (speechTrigger == true && Camera.main.transform.position == OldManRoomCameraPos) {
+			counter--;
+			if (counter == 0)
+				counter = 8;
+			if (counter == 8 && textArr.Length > idxOfText) {
+				print (textArr [idxOfText]);
+				if (idxOfText == 19) {
+					this.GetComponent<TextMesh> ().text += "\n          "; 
+				}
+				this.GetComponent<TextMesh> ().text += textArr [idxOfText];
+				idxOfText++;
+			}
+		} else {
+			idxOfText=0;
+			this.GetComponent<TextMesh> ().text = "";
+			counter = 8;
+		}
+	}
+}
---
+++ Assets/Scripts/SpeechDriver.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeechDriver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider coll) {
+		if (coll.gameObject.tag == "Link" && PlayerControl.instance.current_direction == Direction.WEST) {
+			OldManRoomSpeech.instance.speechTrigger = true;
+		} else {
+			OldManRoomSpeech.instance.speechTrigger = false;
+		}
+	}
+}
